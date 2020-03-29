c22147486aad1ecd8069d4ad576d4c7eae2aefae
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2020-03-29 14:08:44 -0300

    Include test files in SVC

---
+++ test/does-not-need-to-add-block/output.js
@@ -0,0 +1,13 @@
+const _filterFn = (i) => i > 1
+
+const _mapFn = (i) => i * 2
+
+const _reducer = (curr, next) => {
+  if (_filterFn(next)) {
+    curr.push(_mapFn(next, curr.length))
+  }
+
+  return curr
+}
+
+;[1, 2, 3].reduce(_reducer, [])
